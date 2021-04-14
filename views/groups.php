0defd77761977eda765a4c1c7dc8d803c72c3429
DYarmots <DYarmots@gmail.com>
2021-04-14 19:59:53 +0300

    old project upload

--- views/groups.php
+++ views/groups.php
@@ -1,56 +1,56 @@
 <?php 
-
-groups_list();
-
-groups_add();
-
 function groups_add(){
-	$sql = 'INSERT INTO (caption) VALUES ('.$_GET['caption'].')';
-	?>
-	<form>
-  <div class="form-group">
-    <label>Название группы</label>
-    <input type="text" class="form-control" placeholder="Введите название группы">
-  </div>
+  include('connect.php');
+  $sql = 'INSERT INTO (caption) VALUES ('.$_POST['caption'].')';
+  if (!mysqli_query($link, $sql)) {
+    echo "Error: " . $sql . "<br>" . mysqli_error($link);
+  }
+  mysqli_close($link);
+  ?>
+  <form>
+    <div class="form-group">
+      <label>Название группы</label>
+      <input type="text" name="caption" class="form-control" placeholder="Введите название группы">
+    </div>
 
-  <button type="submit" class="btn btn-primary">Сохранить</button>
-</form>
-<?
+    <button type="submit" class="btn btn-primary">Сохранить</button>
+  </form>
+  <?
 }
 
 function groups_list(){
 	?>
-      <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-	<?
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Larry</td>
+        <td>the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>
+  <?
 }
 
- ?>
+?>
